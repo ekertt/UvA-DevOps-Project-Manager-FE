59f4982d485b4cf4aed94cdfb4e41fe12c82a6e4
Tim van Ekert <tim.van.ekert@hva.nl>
2023-03-01 12:02:51 +0100

    feat: scrum dashboard styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import type { FC } from "react";
-import { Button } from "antd";
 import "antd/dist/reset.css";
 import "./App.css";
+import ScrumBoardDashboard from "./views/ScrumBoardDashboard";
 
 const App: FC = () => (
   <div className="App">
-    <header className="App-header">
-      <Button type="primary">Ant Design button</Button>
-    </header>
+    <ScrumBoardDashboard />
   </div>
 );
 
