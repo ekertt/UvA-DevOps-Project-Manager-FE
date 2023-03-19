3d76cd347c87c0ba8b49989745eaf854feba2f92
Tim van Ekert <tim.van.ekert@hva.nl>
2023-03-19 15:59:32 +0100

    feat: CRUD implemented for projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import type { FC } from 'react';
 import React, { useState } from 'react';
 import 'antd/dist/reset.css';
 import './App.css';
-import ScrumBoardDashboard from './views/ScrumBoardDashboard';
 import { Login } from './views/Login';
 import authContext from './auth/auth-context';
 import { SignInUserSession } from './models/sign-in-user-session';
+import {Projects} from "./views/Projects";
 
 const App: FC = () => {
   const [user, setUser] = useState<SignInUserSession | undefined>(undefined);
 
+
   return (
     <div className="App">
       <authContext.Provider value={{ user, setUser }}>
-        {!user ? <Login /> : <ScrumBoardDashboard />}
+        {!user ? <Login /> : <Projects />}
       </authContext.Provider>
     </div>
   );
