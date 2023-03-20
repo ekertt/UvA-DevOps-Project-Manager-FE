4d40e0648a56a09dc5f63359280eaec20f11b8f4
René Kok <34970901+rkok98@users.noreply.github.com>
2023-03-20 16:07:23 +0100

    fix: refactor and fix update bug (#25)
    * fix: refactor and fix update bug
    
    * fix: refactor and fix update bug

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,69 +1,61 @@
 import { FC, useContext, useEffect, useState } from 'react';
-import { Button, Form, Input, List, Modal, Popconfirm, Typography } from 'antd';
+import { Button, List, Modal, Popconfirm, Space, Typography } from 'antd';
 import {
-  PlusOutlined,
   DeleteOutlined,
   EditOutlined,
   EyeOutlined,
+  PlusOutlined,
+  ReloadOutlined,
 } from '@ant-design/icons';
 import { Project } from '../models/Project';
-import {
-  createProject,
-  deleteProject,
-  getProjects,
-  updateProject,
-} from '../api/projects';
+import { deleteProject, getProjects } from '../api/projects';
 import authContext from '../auth/auth-context';
-import { Link } from 'react-router-dom';
+import { EditProjectModal } from './edit-project-modal';
+import { CreateProjectModal } from './create-project-modal';
 
 const { Title } = Typography;
 
 export const Projects: FC = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [isCreateModalVisible, setCreateModalVisibility] =
+    useState<boolean>(false);
+  const [isEditModalVisible, setEditModalVisibility] = useState<boolean>(false);
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
-  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
-  const [form] = Form.useForm();
+
   const [projects, setProjects] = useState<Project[]>([]);
 
   const { user } = useContext(authContext);
 
-  const handleGetProjects = async () => {
-    const projects = await getProjects(user!.idToken.jwtToken);
-    setProjects(projects);
+  const handleOpenCreateModal = () => {
+    setCreateModalVisibility(true);
   };
 
-  const handleCreateProject = async (project: {
-    name: string;
-    description: string;
-  }) => {
-    try {
-      await createProject(project, user!.idToken.jwtToken);
-      setIsModalVisible(false);
-      form.resetFields();
-      await handleGetProjects();
-    } catch (error) {
-      console.log(error);
-    }
+  const handleOpenEditModal = (project: Project) => {
+    setSelectedProject(project);
+    setEditModalVisibility(true);
+  };
+
+  const handleCloseModal = () => {
+    setEditModalVisibility(false);
+    setCreateModalVisibility(false);
+  };
+
+  const handleCreateUpdateModal = async () => {
+    handleCloseModal();
+    await handleGetProjects();
+  };
+
+  const handleGetProjects = async () => {
+    setIsLoading(true);
+    const projects = await getProjects(user!.idToken.jwtToken);
+    setProjects(projects);
+    setIsLoading(false);
   };
 
   const handleDeleteProject = async (projectId: string) => {
-    try {
-      await deleteProject(projectId, user!.idToken.jwtToken);
-      await handleGetProjects();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleEditProject = async (project: Project) => {
-    try {
-      await updateProject(project, user!.idToken.jwtToken);
-      setSelectedProject(null);
-      setIsEditModalVisible(false);
-      await handleGetProjects();
-    } catch (error) {
-      console.log(error);
-    }
+    await deleteProject(projectId, user!.idToken.jwtToken);
+    await handleGetProjects();
   };
 
   useEffect(() => {
@@ -83,106 +75,50 @@ export const Projects: FC = () => {
         <div
           style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}
         >
-          <Button
-            type="primary"
-            onClick={() => setIsModalVisible(true)}
-            icon={<PlusOutlined />}
-          >
-            Create Project
-          </Button>
+          <Space>
+            <Button
+              onClick={handleGetProjects}
+              icon={<ReloadOutlined />}
+              loading={isLoading}
+            />
+            <Button
+              type="primary"
+              onClick={handleOpenCreateModal}
+              icon={<PlusOutlined />}
+            >
+              Create Project
+            </Button>
+          </Space>
         </div>
       </div>
-      <Modal
-        title="Create Project"
-        open={isModalVisible}
-        onCancel={() => setIsModalVisible(false)}
-        footer={null}
-      >
-        <Form form={form} onFinish={handleCreateProject} layout="vertical">
-          <Form.Item
-            name="name"
-            label="Name"
-            rules={[
-              {
-                required: true,
-                message: 'Please enter a name for your project',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="description"
-            label="Description"
-            rules={[
-              {
-                required: true,
-                message: 'Please enter a description for your project',
-              },
-            ]}
-          >
-            <Input.TextArea />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Create
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
 
       <Modal
         title="Edit Project"
         open={isEditModalVisible}
-        onCancel={() => setIsEditModalVisible(false)}
+        onCancel={handleCloseModal}
+        onOk={handleCloseModal}
         footer={null}
+        destroyOnClose
       >
-        <Form
-          form={form}
-          initialValues={{
-            name: selectedProject?.name,
-            description: selectedProject?.description,
-          }}
-          onFinish={() => {
-            form.validateFields().then((values) => {
-              handleEditProject({ ...selectedProject, ...values });
-            });
-          }}
-          layout="vertical"
-        >
-          <Form.Item
-            name="name"
-            label="Name"
-            rules={[
-              {
-                required: true,
-                message: 'Please enter a name for your project',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="description"
-            label="Description"
-            rules={[
-              {
-                required: true,
-                message: 'Please enter a description for your project',
-              },
-            ]}
-          >
-            <Input.TextArea />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Save
-            </Button>
-          </Form.Item>
-        </Form>
+        <EditProjectModal
+          project={selectedProject}
+          onUpdate={handleCreateUpdateModal}
+        />
       </Modal>
+
+      <Modal
+        title="Create Project"
+        open={isCreateModalVisible}
+        onCancel={handleCloseModal}
+        onOk={handleCloseModal}
+        footer={null}
+        destroyOnClose
+      >
+        <CreateProjectModal onCreate={handleCreateUpdateModal} />
+      </Modal>
+
       <List
-        itemLayout="horizontal"
+        loading={isLoading}
         dataSource={projects}
         renderItem={(project) => (
           <List.Item>
@@ -190,28 +126,24 @@ export const Projects: FC = () => {
               title={project.name}
               description={project.description}
             />
-            <Popconfirm
-              title="Are you sure you want to delete this project?"
-              onConfirm={() => handleDeleteProject(project.id)}
-              okText="Yes"
-              cancelText="No"
-            >
-              <Button danger icon={<DeleteOutlined />}>
-                Delete
-              </Button>
-            </Popconfirm>
-            <Button
-              icon={<EditOutlined />}
-              onClick={() => {
-                setSelectedProject(project);
-                setIsEditModalVisible(true);
-              }}
-            >
-              Edit
-            </Button>
-            <Link to={`/projects/${project.id}`}>
-              <Button icon={<EyeOutlined />}>View</Button>
-            </Link>
+            <Space>
+              <Popconfirm
+                id={`${project.id}-delete-confirm`}
+                title={`Are you sure you want to delete ${project.name}?`}
+                onConfirm={() => handleDeleteProject(project.id)}
+              >
+                <Button danger icon={<DeleteOutlined />} />
+              </Popconfirm>
+              <Button
+                icon={<EditOutlined />}
+                onClick={() => handleOpenEditModal(project)}
+              />
+              <Button
+                type="primary"
+                icon={<EyeOutlined />}
+                href={`/projects/${project.id}`}
+              />
+            </Space>
           </List.Item>
         )}
       />
