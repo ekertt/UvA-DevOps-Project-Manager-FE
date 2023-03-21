5e0577575b8cbb47a3d366240b8a5f088e92f361
ekertt <t.v.ekert@gmail.com>
2023-03-21 20:37:08 +0100

    fix: header optimization

--- src/views/NotAuthenticated.tsx
+++ src/views/NotAuthenticated.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Space, Typography } from 'antd';
+import { Button, Divider, Layout, Space, Typography } from 'antd';
 import React, { FC, useContext, useEffect } from 'react';
 import { Auth, Hub } from 'aws-amplify';
 import { LoginOutlined } from '@ant-design/icons';
@@ -6,6 +6,7 @@ import { ProCard } from '@ant-design/pro-components';
 import authContext from '../auth/auth-context';
 import { SignInUserSession } from '../models/sign-in-user-session';
 import { ReactComponent as LandingPageSVG } from '../assets/not-authenticated.svg';
+import { TitleComponent } from './TitleComponent';
 
 const { Title } = Typography;
 
@@ -30,54 +31,62 @@ export const NotAuthenticated: FC = () => {
   );
 
   return (
-    <div
-      style={{
-        backgroundColor: '#8BC6EC',
-        backgroundImage: 'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)',
-        height: '100vh',
-      }}
-    >
-      <ProCard
-        direction="column"
-        ghost
-        layout="center"
-        style={{ height: '100%' }}
+    <>
+      <TitleComponent />
+      <div
+        style={{
+          backgroundColor: '#8BC6EC',
+          backgroundImage: 'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)',
+          height: '100vh',
+        }}
       >
-        <ProCard direction="row" bordered colSpan={8}>
-          <ProCard ghost layout="center" colSpan={8} style={{ height: '100%' }}>
-            <LandingPageSVG />
-          </ProCard>
-          <Divider type="vertical" />
-          <ProCard>
-            <Title>Welcome to SprintVision</Title>
-            <Title level={4}>
-              The ultimate online scrum board with AI category recognition
-              built-in.
-            </Title>
-            <p style={{ marginBottom: 20, paddingTop: 15 }}>
-              Sign up now to streamline your team's workflow and improve
-              productivity. With SprintVision, you can easily manage tasks,
-              track progress, and categorize work items with just a few clicks.
-              Our powerful AI technology takes care of categorization, leaving
-              you more time to focus on what really matters - delivering your
-              project on time and within budget.
-            </p>
-            <Space>
-              <Button
-                type="primary"
-                size="large"
-                onClick={() => Auth.federatedSignIn()}
-              >
-                <LoginOutlined />
-                Create an account
-              </Button>
-              <Button type="link" onClick={() => Auth.federatedSignIn()}>
-                or login
-              </Button>
-            </Space>
+        <ProCard
+          direction="column"
+          ghost
+          layout="center"
+          style={{ height: '100%' }}
+        >
+          <ProCard direction="row" bordered colSpan={8}>
+            <ProCard
+              ghost
+              layout="center"
+              colSpan={8}
+              style={{ height: '100%' }}
+            >
+              <LandingPageSVG />
+            </ProCard>
+            <Divider type="vertical" />
+            <ProCard>
+              <Title>Welcome to SprintVision</Title>
+              <Title level={4}>
+                The ultimate online scrum board with AI category recognition
+                built-in.
+              </Title>
+              <p style={{ marginBottom: 20, paddingTop: 15 }}>
+                Sign up now to streamline your team's workflow and improve
+                productivity. With SprintVision, you can easily manage tasks,
+                track progress, and categorize work items with just a few
+                clicks. Our powerful AI technology takes care of categorization,
+                leaving you more time to focus on what really matters -
+                delivering your project on time and within budget.
+              </p>
+              <Space>
+                <Button
+                  type="primary"
+                  size="large"
+                  onClick={() => Auth.federatedSignIn()}
+                >
+                  <LoginOutlined />
+                  Create an account
+                </Button>
+                <Button type="link" onClick={() => Auth.federatedSignIn()}>
+                  or login
+                </Button>
+              </Space>
+            </ProCard>
           </ProCard>
         </ProCard>
-      </ProCard>
-    </div>
+      </div>
+    </>
   );
 };
---
+++ src/views/TitleComponent.tsx
@@ -0,0 +1,44 @@
+import { Typography } from 'antd';
+import React, { FC } from 'react';
+
+const { Title } = Typography;
+export const TitleComponent: FC = () => {
+  return (
+    <>
+      <div
+        style={{
+          background: 'transparent',
+          padding: '10px 16px',
+          position: 'absolute',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Title
+            level={2}
+            style={{
+              color: '#fff',
+              textTransform: 'uppercase',
+              letterSpacing: '0.15em',
+              fontWeight: 'bold',
+              fontSize: '2em',
+              textShadow: '2px 2px 0px #333',
+              transform: 'skew(-10deg)',
+              marginRight: '20px',
+              width: '100%',
+            }}
+          >
+            SPRINTVISION
+            <span
+              style={{
+                marginLeft: '-5px',
+                textShadow: '2px 2px 0px #333',
+              }}
+            />
+          </Title>
+        </div>
+      </div>
+    </>
+  );
+};
